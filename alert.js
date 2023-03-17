d0014b8e1e7ba02fc5f5ae1c8392eaff8a17bbfd
Janovir <33178075+Janovir@users.noreply.github.com>
2023-03-17 14:50:45 +0000

    Update alert.js
    xml http request content

--- alert.js
+++ alert.js
@@ -1,1 +1,7 @@
-alert('myXss');
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://api.countapi.xyz/hit/janovir/visits");
+xhr.responseType = "json";
+xhr.onload = function() {
+    document.getElementById('visits').innerText = this.response.value;
+}
+xhr.send();
